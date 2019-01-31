41712af6f46e1304e3f50f238811e28b91827246
Ahmed Awadallah <aawadall@ualberta.ca>
2019-01-31 16:37:42 -0500

    initial

---
+++ main.py
@@ -0,0 +1,17 @@
+import base64
+
+
+def hello_pubsub(event, context):
+    """Triggered from a message on a Cloud Pub/Sub topic.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    msg_details = base64.b64decode(event['attributes'])
+
+    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+    print(pubsub_message)
+    print("Message Details:")
+    for det in msg_details:
+        print(det.decode('utf-8'))
+
