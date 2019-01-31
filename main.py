fa58dff8ed990d0892464d65b75d1397a7fe13bd
Ahmed Awadallah <aawadall@ualberta.ca>
2019-01-31 16:46:43 -0500

    remove details, test if works

--- main.py
+++ main.py
@@ -7,11 +7,7 @@ def hello_pubsub(event, context):
          event (dict): Event payload.
          context (google.cloud.functions.Context): Metadata for the event.
     """
-    msg_details = event['attributes']
 
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
     print(pubsub_message)
-    print("Message Details:")
-    for det in msg_details:
-        print(det.decode('utf-8'))
 
