c01d5b47a0860b62fdbc533e022bfee36986114d
aawadall <aawadall@ualberta.ca>
2019-02-02 11:42:37 -0500

    one more push to sync client and server

--- run_client.sh
+++ run_client.sh
@@ -1,2 +1,2 @@
 export PATH=$PATH:$PWD/bin
-go run src/google.golang.org/grpc/examples/helloworld/greeter_client/main.go
+go run src/google.golang.org/grpc/examples/helloworld/greeter_client/main.go+
