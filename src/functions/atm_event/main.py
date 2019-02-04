2259b01bdad7e7ba46a89f8c4b9b5b3d79898de7
Ahmed Awadallah <aawadall@ualberta.ca>
2019-02-04 16:54:01 -0500

    and the other decode

--- src/functions/atm_event/main.py
+++ src/functions/atm_event/main.py
@@ -3,7 +3,7 @@ def atm_event(data, context):
 
     print(base64.b64decode(data))
     if 'data' in data:
-        name = base64.b64decode(data['data'].decode('utf-8'))
+        name = base64.b64decode(data['data'])
     else:
         name = 'Default Name'
     print('Name: {}'.format(name))
