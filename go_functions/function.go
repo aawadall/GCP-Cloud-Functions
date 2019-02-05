cb19c6553fe1988e02bf02256641795f9a42a23c
Ahmed Awadallah <aawadall@ualberta.ca>
2019-02-05 15:24:51 -0500

    attempt to build json response

--- go_functions/function.go
+++ go_functions/function.go
@@ -11,6 +11,19 @@ import (
 // HelloWorld prints the JSON encoded "message" field in the body
 // of the request or "Hello, World!" if there isn't one.
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	
+	type Event struct {
+		EventClass 	string
+		TerminalID	string 
+		Notes		string
+	}
+
+	event := Event{
+		EventClass: "cash_swap",
+		TerminalID: "ATM0000",
+		Notes: "TBC"
+	}
+
 	var d struct {
 		Message string `json:"message"`
 	}
@@ -19,7 +32,8 @@ func HelloWorld(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	if d.Message == "" {
-		fmt.Fprint(w, "dMsg:"+r.Method)
+		w, err := json.Marshal(event)	
+		//fmt.Fprint(w, "dMsg:"+r.Method)
 		return
 	}
 	fmt.Fprint(w, html.EscapeString(d.Message))
