baf73bde9cbccb9005c0bf04e40a5469ea7fc144
Ahmed Awadallah <aawadall@ualberta.ca>
2019-02-05 14:21:53 -0500

    force method

--- go_functions/function.go
+++ go_functions/function.go
@@ -15,7 +15,7 @@ func HelloWorld(w http.ResponseWriter, r *http.Request) {
 		Message string `json:"message"`
 	}
 	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-		fmt.Fprint(w, "Hello World!")
+		fmt.Fprint(w, r.Method)
 		return
 	}
 	if d.Message == "" {
